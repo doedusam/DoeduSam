a90b9570e6d47df3de82d115a3516f84b0e47da1
dsa das <vovicus01@mail.ru>
2020-12-18 01:51:45 +0400

    Huinya

--- DEM/MainWindow.xaml.cs
+++ DEM/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace DEM
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
 }
